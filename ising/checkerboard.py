a9dffeba6a39eef5c66ef17cc67511b784487552
David Chen <nocktavius@gmail.com>
2023-12-04 11:54:10 +0100

    added most of the checkerboard algorithm helper functions

--- ising/checkerboard.py
+++ ising/checkerboard.py
@@ -1,42 +1,55 @@
 import numpy as np
 from numba import njit, prange
 
+
+SquareLattice = np.ndarray[int, int]
+
 WhiteSquares = str
 BlackSquares = str
 SquareColor = {WhiteSquares, BlackSquares}
 
 
-def checkerboard_weave(
-    lattice: np.ndarray[int, int], square_color: SquareColor
-) -> np.ndarray[int, int]:
-    """Alternate an array with zeros, put the values of the array either
-    on the WhiteSquares or the BlackSquares according to checkboard convention."""
+def unweave_checkerboard(lattice: SquareLattice) -> tuple[SquareLattice, SquareLattice]:
+    """Split lattice into its black squares and white squares."""
 
-    assert square_color in ("w", "white", "b", "black")
+    n, m = lattice.shape
+
+    assert m % 2 == 0, "Cannot unweave uneven checkerboard; uneven number or columns."
+
+    mask = np.indices((n, m)).sum(axis=0) % 2 == 0
+
+    white_lattice = lattice[mask].reshape((n, m // 2))
+    black_lattice = lattice[~mask].reshape((n, m // 2))
+
+    return (white_lattice, black_lattice)
+
+
+def weave_checkerboard(white_lattice, black_lattice) -> SquareLattice:
+    """Weave together two lattices of the same size."""
+
+    assert (
+        white_lattice.shape == black_lattice.shape
+    ), "Cannot weave together differnently shaped lattices; result not a rectangular lattice"
+
+    return _weave_checkerboard(white_lattice, "w") + _weave_checkerboard(
+        black_lattice, "b"
+    )
+
+
+def _weave_checkerboard(
+    lattice: SquareLattice, square_color: SquareColor
+) -> SquareLattice:
+    """Create an array with double the number of columns, where the values are
+     alternatingly values from the lattice and zeros, put the values of the array either
+    on the WhiteSquares or the BlackSquares according to checkboard convention."""
 
     n, m = lattice.shape
 
     if square_color in ("w", "white"):
-        return lattice.repeat(2, axis=1) * (
-            np.ones((n, 2 * m), dtype=np.int32) - np.indices((n, 2 * m)).sum(axis=0) % 2
-        )
+        return lattice.repeat(2, axis=1) * (np.indices((n, 2 * m)).sum(axis=0) % 2 == 0)
 
     if square_color in ("b", "black"):
-        return lattice.repeat(2, axis=1) * (np.indices((n, 2 * m)).sum(axis=0) % 2)
-
-
-def combine_checkerboard_lattices(
-    black_lattice: np.ndarray[int, int], white_lattice: np.ndarray[int, int]
-) -> np.ndarray[int, int]:
-    """Combine the black_lattice and white_lattice into a single
-    lattice using the checkerboard convention that top-left corner / bottom-right corner is white.
-    Note that the dimensions of the black squares and the white squares need to be equal.
-    """
-    assert black_lattice.shape == white_lattice.shape
-
-    return checkerboard_weave(black_lattice, "black") + checkerboard_weave(
-        white_lattice, "white"
-    )
+        return lattice.repeat(2, axis=1) * (np.indices((n, 2 * m)).sum(axis=0) % 2 == 1)
 
 
 def generate_random_array(n, m):

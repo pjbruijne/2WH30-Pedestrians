75eb3053f7c485e0c2227f924faed4113787aa6c
pjbruijne <p.j.d.bruijne@student.tue.nl>
2023-12-18 14:59:46 +0100

    WIP correlation coefficient stuff

--- ising/data_analysis.py
+++ ising/data_analysis.py
@@ -29,3 +29,68 @@ def getCorrelationCoefficient_2(node:tuple[int,...], simulation:list[np.ndarray[
         expSpinProduct /= len(simulation)
         return expSpinProduct - (expSpin**2)
     else: pass
+    
+def getCorrelationCoefficient_3(simulation:list[np.ndarray[int,int]], useWrap:bool|None=False) -> np.ndarray[float]:
+    """
+    Based on the stackexchange post 
+    https://physics.stackexchange.com/questions/169442/how-to-measure-the-spin-spin-correlation-in-a-monte-carlo-simulation-of-the-isin.\n
+    Assumes that the lattice is square, not simply rectangular.
+    """
+    width, height = simulation[0].shape
+    assert width == height
+    L = width
+    result = np.zeros(L,dtype=np.float64)
+    for lattice in simulation:
+        for r in range(L):
+            samples = 0
+            sum = 0
+            for i in range(L):   
+                if r <= i:
+                    sum += lattice[i,i]*(lattice[i-r,i] + lattice[i,i-r])
+                    samples += 2
+                if i < L-r:
+                    sum += lattice[i,i]*(lattice[i+r,i] + lattice[i,i+r])
+                    samples += 2
+            result[r] += sum / samples
+    result /= len(simulation)-1
+    return result
+
+def getCorrelationCoefficient_4(node:tuple[int,int], simulation:list[np.ndarray[int,int]]) -> np.ndarray[float]:
+    width, height = simulation[0].shape
+    x,y = node
+    maxR = max(abs(width-x),abs(height-y))
+    result = np.zeros(maxR,dtype=np.float64)
+    meanS0 = 0
+    meanS1 = np.zeros(maxR, dtype=np.float64)
+    meanS1_samples = 0
+    for lattice in simulation:
+        meanS0 += lattice[x,y] 
+        for r in range(maxR):
+            samples = 0
+            sum = 0 
+            if r <= x:
+                sum += lattice[x,y]*lattice[x-r,y]
+                meanS1[r] += lattice[x-r,y]
+                samples += 1
+                meanS1_samples += 1
+            if r <= y:
+                sum += lattice[x,y]*lattice[x,y-r]
+                meanS1[r] += lattice[x,y-r]
+                samples += 1
+                meanS1_samples += 1
+            if x < width-r:
+                sum += lattice[x,y]*lattice[x+r,y]
+                meanS1[r] += lattice[x+r,y]
+                samples += 1
+                meanS1_samples += 1
+            if y < height - r:
+                sum += lattice[x,y]*lattice[x,y+r]
+                meanS1[r] += lattice[x,y+r]
+                samples += 1
+                meanS1_samples += 1
+            result[r] += sum / samples
+    result /= len(simulation)-1
+    meanS0 /= len(simulation)
+    meanS1 /= meanS1_samples
+    result2 = result - meanS0 * meanS1
+    return result,result2
--- ising/lattice_objects.py
+++ ising/lattice_objects.py
@@ -2,9 +2,11 @@ import os
 import shutil
 
 from abc import ABC, abstractmethod
+from typing import Literal
 import numpy as np
 from numba import njit
 from PIL import Image
+from scipy.ndimage import convolve
 #from ipywidgets import interact
 
 
@@ -12,13 +14,13 @@ class lattice(ABC):
     
     @njit()
     @abstractmethod
-    def neighbor_sum(self, X:tuple[int,...]):
+    def neighbor_sum(self, X:tuple[int,...]) -> int:
         """Calculates the sum of the neighbours of the spin at position (x1,x2,...). Implementation depends on subclass."""
         pass
     
     @njit()
     @abstractmethod
-    def simulate(self, J: float, B: float, T: float, steps: int):
+    def simulate(self, J: float, B: float, T: float, steps: int, method: Literal["metropolis","checkerboard"] | None = "metropolis"):
         """Simulate ising model using the Metropolis algorithm.
         J : global spin-spin interaction constant,
         B : external magnetic field,
@@ -49,6 +51,11 @@ class lattice(ABC):
 
         return [self.upscale_lattice(matrix, scale_factor) for matrix in matrices]
     
+    @njit()
+    @abstractmethod
+    def repeated_simulations(self, J: float, B: float, T: float, steps: int, runs: int, method: Literal["metropolis","checkerboard"] | None = "metropolis") -> list[np.ndarray[int,int]]:
+        pass
+    
 class lattice2d(lattice):
     
     def __init__(self, width: int, height: int, states: list[int] = [-1,1]) -> None:
@@ -56,8 +63,15 @@ class lattice2d(lattice):
         self.matrix:np.ndarray[int,int] = np.random.choice(states, size=(width, height))
         self.initialmatrix = self.matrix.copy()
         self.simulation:list[np.ndarray[int,int]] = []
+        self.simulation_finals:list[np.ndarray[int,int]] = []
     
-    def simulate(self, J: float, B: float, T: float, steps: int):
+    def simulate(self, J: float, B: float, T: float, steps: int, method: Literal["metropolis","checkerboard"] | None = "metropolis") -> list[np.ndarray[int,int]]:
+        if method == "metropolis":
+            return self.__metropolis(J,B,T,steps)
+        if method == "checkerboard":
+            return self.__checkerboard(J,B,T,steps)
+
+    def __metropolis(self, J, B, T, steps) -> list[np.ndarray[int,int]]:
         self.simulation.clear()
         self.simulation.append(self.initialmatrix)
         self.matrix = self.initialmatrix.copy()
@@ -67,7 +81,7 @@ class lattice2d(lattice):
             x = np.random.randint(width)
             y = np.random.randint(height)
 
-            spin = self.matrix[x, y]
+            spin:int = self.matrix[x, y]
 
             position = (x,y)
             neighbour_sum = self.neighbor_sum(position)
@@ -80,11 +94,102 @@ class lattice2d(lattice):
             self.simulation.append(self.matrix.copy())
         return self.simulation
 
+    def __checkerboard(self, J, B, T, steps) -> list[np.ndarray[int,int]]:
+        """Simulate evolution of a lattice using the checkerboard algorithm.
+        This simulation algorithm converges faster than the default metropolis algorithm."""
+
+        if steps % 2 == 1:
+            steps += 1
+
+        n, m = self.matrix.shape
+        self.simulation.clear()
+        self.matrix = self.initialmatrix.copy()
+        self.simulation.append(self.matrix)
+
+        for i in range(1, steps + 1, 2):
+            # update the white squares
+            dE_white, _ = self.__unweave_checkerboard(self.__hamiltonian_constant_J(self.matrix, J, B))
+
+            mask = self.__generate_random_array(n, m // 2) < np.exp(-dE_white / T)
+            mask = self.___weave_checkerboard(mask, "white")
+
+            self.matrix[mask] = -self.matrix[mask]
+            self.simulation.append(self.matrix)
+
+            # update the black squares
+            _, dE_black = self.__unweave_checkerboard(self.__hamiltonian_constant_J(self.matrix, J, B))
+
+            mask = self.__generate_random_array(n, m // 2) < np.exp(-dE_black / T)
+            mask = self.___weave_checkerboard(mask, "black")
+
+            self.matrix[mask] = -self.matrix[mask]
+            self.simulation.append(self.matrix)
+
+        return self.simulation
+
+    def __unweave_checkerboard(self, lattice: np.ndarray[int,int]) -> tuple[np.ndarray[int,int], np.ndarray[int,int]]:
+        """Split lattice into its white squares and black squares according to the checkerboard convention."""
+        n, m = lattice.shape
+
+        assert m % 2 == 0, "Cannot unweave uneven checkerboard; uneven number or columns."
+
+        mask = np.indices((n, m)).sum(axis=0) % 2 == 0
+
+        white_lattice = lattice[mask].reshape((n, m // 2))
+        black_lattice = lattice[~mask].reshape((n, m // 2))
+
+        return (white_lattice, black_lattice)
+
+    def __weave_checkerboard(self, white_lattice:np.ndarray[int,int], black_lattice:np.ndarray[int,int]) -> np.ndarray[int,int]:
+        """Weave together two lattices (of the same size) into their combined checkerboard lattice."""
+
+        assert (
+            white_lattice.shape == black_lattice.shape
+        ), "Cannot weave together differnently shaped lattices; result not a rectangular lattice."
+
+        return self.___weave_checkerboard(white_lattice, "w") + self.___weave_checkerboard(
+            black_lattice, "b"
+        )
+
+    def ___weave_checkerboard(self, lattice: np.ndarray[int,int], square_color: {"w","b"}) -> np.ndarray[int,int]:
+        """Create an array with double the number of columns, where the values are
+        alternatingly values from the lattice and zeros, put the values of the array either
+        on the WhiteSquares or the BlackSquares according to checkboard convention."""
+
+        n, m = lattice.shape
+
+        if square_color in ("w", "white"):
+            return lattice.repeat(2, axis=1) * (np.indices((n, 2 * m)).sum(axis=0) % 2 == 0)
+
+        if square_color in ("b", "black"):
+            return lattice.repeat(2, axis=1) * (np.indices((n, 2 * m)).sum(axis=0) % 2 == 1)
+
+    def __generate_random_array(self, n, m):
+        """Create an n by m array with samples from ~Unif [0, 1)."""
+        return np.random.random_sample((n, m))
+
+    def __checkerboard_neighbor_sum(self,lattice:np.ndarray[int,int]) -> np.ndarray[int,int]:
+        kernel = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+
+        return convolve(input=lattice, weights=kernel, mode="wrap")
+
+    def __hamiltonian_constant_J(self, lattice, J, h):
+        """Compute the energy of a lattice."""
+
+        return 2 * (J * self.__checkerboard_neighbor_sum(lattice) + h) * lattice
+
     def lattices_to_images(self, scale_factor: int = 1) -> list[Image.Image]:
         return [self.lattice_to_image(matrix, scale_factor) for matrix in self.simulation]
 
     def upscale_lattice(self, matrix: np.ndarray[int, int], scale_factor: int) -> np.ndarray[int, int]:
         return matrix.repeat(scale_factor, axis=0).repeat(scale_factor, axis=1)
+    
+    def repeated_simulations(self, J: float, B: float, T: float, steps: int, runs: int, method: Literal["metropolis","checkerboard"] | None = "metropolis") -> list[np.ndarray[int,int]]:
+        self.simulation_finals.clear()
+        for i in range(runs):
+            sim = self.simulate(J, B, T, steps, method)
+            self.simulation_finals.append(sim[-1])
+        return self.simulation_finals
     
 class squarelattice2d(lattice2d):
     def neighbor_sum(self, X: tuple[int,...]):
@@ -101,7 +206,7 @@ class squarelattice2d(lattice2d):
             sum += self.matrix[a, b - 1]
         if b < height - 1:  # a lower beighbour exists
             sum += self.matrix[a, b + 1]
-
+        
         return sum
     
     def lattice_to_image(self, matrix: np.ndarray[int, int], scale_factor: int = 1) -> Image:
